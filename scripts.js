7eb866ff67425c4e2bff6a415cd65a06b7b67856
Theo Sperb <theo03.sperb@gmail.com>
2026-04-24 00:24:24 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,41 @@
+let botao = document.querySelector('.botao-gerar');
+let chave = "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";
+let endereco = "https://api.groq.com/openai/v1/chat/completions";
+
+async function gerarCodigo() {
+    let textoUsuario = document.querySelector('.caixa-texto').value;
+    let blocoCodigo = document.querySelector('.bloco-codigo');
+    let resultadoCodigo = document.querySelector('.resultado-codigo');
+
+    let resposta = await fetch(endereco, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": "Bearer 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",
+        }, 
+        body: JSON.stringify({
+            model:"llama-3.3-70b-versatile",
+            messages:  [
+                {
+                role: "system", 
+                content: "Você é um gerador de código HTML e CSS. Responda somente com código puro. NUNCA use crases, markdown ou expliações. Formato: primeiro <style> com o CSS, depois o HTML. Siga EXATAMENTE o que o usuáro pedir. Se pedir algo quicando, use translateY no @keyframes. Se pedir algo girando, use rotate.",
+                },
+                {
+                role: "user",
+                content: textoUsuario
+                }
+            ]
+    
+        })
+    })
+
+    let dados = await resposta.json()
+    let resultado = dados.choices[0].message.content
+
+    blocoCodigo.textContent = resultado;
+    resultadoCodigo.srcdoc = resultado;
+
+
+}    
+
+botao.addEventListener("click", gerarCodigo);
